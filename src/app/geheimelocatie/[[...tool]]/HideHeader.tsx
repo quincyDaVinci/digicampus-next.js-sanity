344bb7c2ce464bf2e7b9d203893dc68cac97ea0b
Quincy Boschman <quincyboschman@outlook.com>
2025-11-05 18:21:49 +0100

    feat: Update HideHeader component to hide both header and footer on Sanity Studio pages

--- src/app/geheimelocatie/[[...tool]]/HideHeader.tsx
+++ src/app/geheimelocatie/[[...tool]]/HideHeader.tsx
@@ -3,20 +3,32 @@
 import { useEffect } from 'react'
 
 /**
- * Client component that hides the site header on Sanity Studio pages
+ * Client component that hides the site header and footer on Sanity Studio pages
  */
 export default function HideHeader() {
   useEffect(() => {
     const headerElement = document.getElementsByTagName('header')
+    const footerElement = document.getElementsByTagName('footer')
+    
     if (headerElement[0]) {
       headerElement[0].style.display = 'none'
     }
+    
+    if (footerElement[0]) {
+      footerElement[0].style.display = 'none'
+    }
 
-    // Cleanup: restore header visibility when component unmounts
+    // Cleanup: restore header and footer visibility when component unmounts
     return () => {
       const headerElement = document.getElementsByTagName('header')
+      const footerElement = document.getElementsByTagName('footer')
+      
       if (headerElement[0]) {
         headerElement[0].style.display = ''
+      }
+      
+      if (footerElement[0]) {
+        footerElement[0].style.display = ''
       }
     }
   }, [])
