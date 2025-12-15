ce6ef7aed45b005a2f8aa0156cf83b2440b5b88a
quincyDaVinci <205070353+quincyDaVinci@users.noreply.github.com>
2025-12-15 15:48:51 +0100

    Ignore Next build output (.next) and remove from index

--- packages/web/.next/dev/server/chunks/ssr/packages_web__next-internal_server_app_[lang]_page_actions_a26750a9.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/packages/web/.next-internal/server/app/[lang]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=packages_web__next-internal_server_app_%5Blang%5D_page_actions_a26750a9.js.map
